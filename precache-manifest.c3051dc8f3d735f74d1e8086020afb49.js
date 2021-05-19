f55ef174f137a234b1a8af34f419b11205703137
anameloni <analuizameloni@gmail.com>
2021-05-19 16:13:54 +0100

    Updates

---
+++ precache-manifest.c3051dc8f3d735f74d1e8086020afb49.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bb278a12561a4af8ceae5ec2717da79",
+    "url": "/NLW2-Oministack/index.html"
+  },
+  {
+    "revision": "19b4abbba4c831787cff",
+    "url": "/NLW2-Oministack/static/css/main.336c1ad2.chunk.css"
+  },
+  {
+    "revision": "7ffe4b0707b9336fad8c",
+    "url": "/NLW2-Oministack/static/js/2.21f9ab4e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/NLW2-Oministack/static/js/2.21f9ab4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19b4abbba4c831787cff",
+    "url": "/NLW2-Oministack/static/js/main.375c5c6e.chunk.js"
+  },
+  {
+    "revision": "2d895f0386dcb5b7f50d",
+    "url": "/NLW2-Oministack/static/js/runtime-main.8821c9d2.js"
+  },
+  {
+    "revision": "1fbb1f8bd147351e311921cdf4e80dc3",
+    "url": "/NLW2-Oministack/static/media/back.1fbb1f8b.svg"
+  },
+  {
+    "revision": "9ed76be57e0f41fb4092cbbcb15e0875",
+    "url": "/NLW2-Oministack/static/media/give-classes.9ed76be5.svg"
+  },
+  {
+    "revision": "c86272eb642793deb29b7eec180c05a5",
+    "url": "/NLW2-Oministack/static/media/landing.c86272eb.svg"
+  },
+  {
+    "revision": "1f9a96a14f86a1ccd682159fe038774d",
+    "url": "/NLW2-Oministack/static/media/logo.1f9a96a1.svg"
+  },
+  {
+    "revision": "e042bb237d1a08daabd12bf0a4fb9780",
+    "url": "/NLW2-Oministack/static/media/purple-heart.e042bb23.svg"
+  },
+  {
+    "revision": "3c22fb01ba7434dfa52b083a67158019",
+    "url": "/NLW2-Oministack/static/media/study.3c22fb01.svg"
+  },
+  {
+    "revision": "744fb7ba0c3df74462352a3858badeb6",
+    "url": "/NLW2-Oministack/static/media/warning.744fb7ba.svg"
+  },
+  {
+    "revision": "c2ffa8a6a2f6690a5eba826c625715ab",
+    "url": "/NLW2-Oministack/static/media/whatsapp.c2ffa8a6.svg"
+  }
+]);
